02db8f68c1acc15058280014cfb31a5e92162e60
Saimirz <106036032+Saimirz@users.noreply.github.com>
2022-12-24 21:51:26 -0500

    Created the basic things needed for the site.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <div className="App">
+           <header>
+            <h1>Zoom Rentals</h1>
+          </header>
+          <main>
+            <form>
+              <label htmlFor="location">Location:</label>
+              <input type="text" id="location" />
+              <label htmlFor="start-date">Start Date:</label>
+              <input type="date" id="start-date" />
+              <label htmlFor="end-date">End Date:</label>
+              <input type="date" id="end-date" />
+              <button type="submit">Search</button>
+            </form>
+            <div className="car-listings">
+              <div className="car-card">
+                <div className="car-details">
+                  <h2>Car Name</h2>
+
+                <label htmlFor="car">Search for Available Cars</label>
+              <input type="text" id="car" />
+                  
+                 
+                </div>
+                
+              </div>
+              {/* Add additional car cards here */}
+            </div>
+          </main>
+        </div>
+
+    
+
+  
+      );
+    }
+    
+    export default App;
+    
